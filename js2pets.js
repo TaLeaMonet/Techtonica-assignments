c599a3cff2f2a51de9cc311f6ca1f9a14e4ec707
S. Marie <sdhmariedesign@gmail.com>
2019-07-24 21:30:17 -0700

    Adds progress on JavaScript 2, part 2

---
+++ js2pets.js
@@ -0,0 +1,21 @@
+let myPets = [ ];
+
+function printCuteAnimals(){
+  var pet1 = "dogs";
+  var pet2 = "cats";
+
+  myPets = [pet1, pet2];
+
+  return myPets.length
+
+}
+
+console.log(printCuteAnimals(myPetsAll));
+/*
+function getAnimals() {
+    return [getFirstValue(), getSecondValue()];
+}
+
+var values = getAnimals();
+var first = values[0];
+var second = values[1];*/
