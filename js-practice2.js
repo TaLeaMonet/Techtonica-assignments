a6248620ac90629084aafe561102c0c613f5c18b
S. Marie <sdhmariedesign@gmail.com>
2019-07-31 07:40:43 -0700

    First commit

---
+++ js-practice2.js
@@ -0,0 +1,7 @@
+//Write a function named loopThrough that takes an argument that is a number and loops through and displays a message “Hello World” that number of times.
+
+function loopThrough(num) {
+  for (i = 1; i <= num; i++) console.log("Hello World!");
+}
+
+loopThrough(7);
