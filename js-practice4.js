6478ec7e1f4f0202380596d4c75049c93c6426cd
S. Marie <sdhmariedesign@gmail.com>
2019-07-31 19:01:35 -0700

    Commits final solution

--- js-practice4.js
+++ js-practice4.js
@@ -4,14 +4,14 @@
 // For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
 function fizzbuzz(num) {
-  i = 0;
+  i = 1;
   while (i <= num) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("Fizz buzz");
+    } else if (i % 3 === 0) {
       console.log("Fizz");
     } else if (i % 5 === 0) {
       console.log("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log("Fizz buzz");
     } else {
       console.log(i);
     }
