1659ed274e4e60725853a3c50cfaa988366dc32d
S. Marie <sdhmariedesign@gmail.com>
2019-07-31 22:42:00 -0700

    Adds progress

--- js-practice6-switch.js
+++ js-practice6-switch.js
@@ -1,45 +1,15 @@
-// Using a switch statement, write a function called sleep_in(weekday, vacation) with these criteria: The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+// // Using a switch statement, write a function called sleep_in(weekday, vacation) with these criteria: The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
-// Results
-// sleep_in(False, False) → True
-
-// sleep_in(True, False) → False
-
-// sleep_in(False, True) → True
-
-let vacation;
-let weekday;
-
-if (weekday === Boolean && vacation === Boolean) {
-  function sleepIn(weekday, vacation) {
-    switch (weekday) {
-      case true:
-        console.log("False");
-        break;
-
-      case false:
-        console.log("True");
-    }
+function sleep_in(weekday, vacation) {
+  switch (true) {
+    case weekday !== true || vacation === true:
+      console.log("True");
+      break;
+    default:
+      console.log("False");
   }
 }
 
-console.log(sleepIn(false, false));
-
-///
-// var weekday;
-// var vacation;
-
-// function sleep_in(weekday, vacation){
-
-//     var weekendVacation = "";
-
-//     switch(sleep_in(false, fasle)){
-
-//         case Boolean: false, false;
-//             weekendVacation.push("True");
-
-//     }
-// console.log(weekendVacation);
-// }
-
-// sleep_in(false, false);
+sleep_in(false, false);
+sleep_in(true, false);
+sleep_in(false, true);
