b10b25f7ee1147bc465e4a9d32f4cd7e51f6c337
S. Marie <sdhmariedesign@gmail.com>
2019-07-31 17:28:10 -0700

    Commits final solution

---
+++ js-practice9.js
@@ -0,0 +1,19 @@
+// Create an object with two key-value pairs.
+
+// Log that object to the console.
+
+// Delete the first key-value pair in the object.
+
+// Log that object to the console again. The first key-value pair should be gone.
+
+var records = {
+  theCure: "Disintegration",
+
+  depecheMode: "World in my Eyes"
+};
+
+console.log(records);
+
+delete records.theCure;
+
+console.log(records);
