e973286c71b231e184464b54e5d57a5481cc5842
S. Marie <sdhmariedesign@gmail.com>
2019-08-09 11:37:37 -0700

    Adds nested function.

--- code-challenge4c-letter-match.js
+++ code-challenge4c-letter-match.js
@@ -1,3 +1,24 @@
+function matchString(stringSet) {
+  let x = [];
+
+  function getLetter(string) {
+    x = string.slice(0, 1);
+    return x;
+  }
+
+  getLetter("anchor");
+
+  for (let i = 0; i <= stringSet.length; i++) {
+    if (x.match(stringSet[i])) {
+      return x.toUpperCase();
+    }
+  }
+}
+
+console.log(matchString(["a", "b", "c", "d", "e"]));
+/*
+FIRST SOLUTION TO TEST FOR RETURNING 1ST LETTER IN CAPS WITH ONE STRINGSET:
+
 function getLetter(string) {
   let x = [];
   x = string.slice(0, 1);
@@ -12,3 +33,4 @@ function getLetter(string) {
 }
 
 getLetter("anchor");
+*/
