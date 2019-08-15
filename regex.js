2db9ded88105779fb9e6944caf0dfddafeae2003
S. Marie <sdhmariedesign@gmail.com>
2019-08-15 15:07:08 -0700

    Fixes ing function.

--- regex.js
+++ regex.js
@@ -28,12 +28,12 @@ console.log(replaceSymantec("This function replaces symantec with semantic."));
 
 // Write a function that takes a string and deletes all words that end in ing?
 
-function deleteIng(input_string) {
-  var ing = input_string.replace(/ing/g, "");
-  return ing;
+function deleteIng(str) {
+  const regex = /\s+\w+(ing)/g;
+  return str.replace(regex, "");
 }
 
-console.log(deleteIng("Painting and listening to records."));
+console.log(deleteIng("I enjoy painting and listening to records."));
 
 // Write a function that takes a string and returns true if it is an email address? Compare your answer with your neighbor.
 
@@ -54,3 +54,14 @@ console.log(emailAddress("My email address is helloworld@helloworld.org."));
 console.log(emailAddress("My email address is helloworld@helloworld.fr."));
 
 // Write a function that takes a string containing names separated by commas like "Leah, Russell, Michelle" and returns an array of names, ["Leah", "Russell", "Michelle"]
+
+// function namesToArray(input_string){
+
+// }
+
+// console.log(namesToArray("Leah, Russell, Michelle"));
+
+var str = "Leah, Russell, Michelle";
+var regexp = str.split(/\s/);
+
+console.log(regexp);
