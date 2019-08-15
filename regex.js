4a369cf41ff5b0bc113928e54ba9d7cbcc3415a2
S. Marie <sdhmariedesign@gmail.com>
2019-08-14 20:03:36 -0700

    First commit

---
+++ regex.js
@@ -0,0 +1,17 @@
+// Write a function that takes a word and returns true if the word ends with tonica
+
+function containsTonica(input_string) {
+  if (input_string.match(/tonica/)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(containsTonica("Techtonica"));
+console.log(containsTonica("sonic"));
+
+// Write a function expression that takes a string replaces all instances of symantec with semantic?
+// Write a function that takes a string and deletes all words that end in ing?
+// Write a function that takes a string and returns true if it is an email address? Compare your answer with your neighbor.
+// Write a function that takes a string containing names separated by commas like "Leah, Russell, Michelle" and returns an array of names, ["Leah", "Russell", "Michelle"]
