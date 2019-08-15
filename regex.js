c09a0e4a5ac2923fd7b378a514758fe66c9c1e40
S. Marie <sdhmariedesign@gmail.com>
2019-08-14 20:14:36 -0700

    Adds second exercise.

--- regex.js
+++ regex.js
@@ -12,6 +12,20 @@ console.log(containsTonica("Techtonica"));
 console.log(containsTonica("sonic"));
 
 // Write a function expression that takes a string replaces all instances of symantec with semantic?
+
+function replaceSymantec(input_string) {
+  var sym = "symantec";
+  var sem = "semantic";
+
+  if (input_string.match(/sym/)) {
+    var newString = input_string.replace(sym, sem);
+  }
+
+  return newString;
+}
+
+console.log(replaceSymantec("This function replaces symantec."));
+
 // Write a function that takes a string and deletes all words that end in ing?
 // Write a function that takes a string and returns true if it is an email address? Compare your answer with your neighbor.
 // Write a function that takes a string containing names separated by commas like "Leah, Russell, Michelle" and returns an array of names, ["Leah", "Russell", "Michelle"]
